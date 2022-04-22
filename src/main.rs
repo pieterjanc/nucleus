7eb6d23ef69a6c035f7fe2f2eccd58bd3e750537
Pieterjan Camerlynck <pieterjan.camerlynck@gmail.com>
2022-04-22 18:08:40 +0200

    hello rust!

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,12 @@ use core::panic::PanicInfo;
 
 core::arch::global_asm!(include_str!("boot.s"));
 
+#[no_mangle]
+fn kernel_init() -> ! {
+    panic!()
+}
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn kernel_panic(_info: &PanicInfo) -> ! {
     loop {}
 }
